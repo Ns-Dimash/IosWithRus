b2e9bb85688b5fa8c43ae25684b4dea3c1e105b9
Dimash Nsanbaev <nsanbaev.15@gmail.com>
2023-02-15 01:07:50 +0600

    Nachalo

--- XO/ViewController.swift
+++ XO/ViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
-
+    
+    private var dimash:UILabel = {
+        let label = UILabel()
+        label.text = "Rustem MAAAAl"
+        label.textColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
+        view.addSubview(dimash)
+        setupConst()
         
     }
+    
+    func setupConst(){
+        dimash.snp.makeConstraints{make in
+            make.center.equalToSuperview()
+        }
+    }
+
 }
 
