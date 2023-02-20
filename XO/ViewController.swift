72b92925062d1c28f3eb3e59607706df4771d1ea
Dimash Nsanbaev <nsanbaev.15@gmail.com>
2023-02-21 02:22:46 +0600

    Roll Dies

--- XO/ViewController.swift
+++ XO/ViewController.swift
@@ -9,25 +9,33 @@ import UIKit
 import SnapKit
 class ViewController: UIViewController {
     
-    private var dimash:UILabel = {
-        let label = UILabel()
-        label.text = "MAAAAl"
-        label.textColor = .white
-        return label
+    let arr = ["roll1","roll2","roll3","roll4","roll5","roll6"]
+    lazy var ran:Array = {
+        return arr
+    }()
+    let random = ran.randomElement()
+    
+    private var backgroundImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(named: "\(random)")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        view.addSubview(dimash)
+        view.addSubview(backgroundImageView)
         setupConst()
-        
+        print(random)
     }
     
     func setupConst(){
-        dimash.snp.makeConstraints{make in
+        backgroundImageView.snp.makeConstraints { make in
             make.center.equalToSuperview()
+            make.width.height.equalTo(150)
         }
+        
     }
 
 }
